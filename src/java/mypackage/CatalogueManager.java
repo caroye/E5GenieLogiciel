7f64d150e765021be6152d89d0fa387e1bc1dac4
KevinBordier <kev.bor.77@gmail.com>
2017-12-04 09:47:09 +0100

    Creation des classes techniques, les methodes sont a remplir

---
+++ src/java/mypackage/CatalogueManager.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mypackage;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Kévin
+ */
+@ManagedBean
+@RequestScoped
+public class CatalogueManager {
+
+    /**
+     * A remplir
+     */
+    public void add(){
+        
+    }
+    
+    public void addMultiple(){
+        
+    }
+    
+    public void find(){
+        
+    }
+    
+    public void getBook(){
+        
+    }
+    
+    public void setBook(){
+        
+    }
+    
+    public void removeBook(){
+        
+    }
+}
---
+++ src/java/mypackage/CommandeManager.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mypackage;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Kévin
+ */
+@ManagedBean
+@RequestScoped
+public class CommandeManager {
+
+    /**
+     * A remplir
+     */
+    public void create(){
+        
+    }
+    
+    public void add(){
+        
+    }
+    
+    public void setCommand(){
+        
+    }
+    
+    public void saveCommande(){
+        
+    }
+    
+    public void deleteCommand(){
+        
+    }
+    
+    public void update(){
+        
+    }
+}
